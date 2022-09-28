10dc09f3824328358557a7200fb0e895f77601e9
sjhony-x <sjhony-x@student.42sp.org.br>
2022-09-28 03:11:02 +0200

    teste revert rotate ok

--- tests/test_reverse_rotate.c
+++ tests/test_reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: sjhony-x <sjhony-x@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 03:25:30 by sjhony-x          #+#    #+#             */
-/*   Updated: 2022/09/28 03:05:44 by sjhony-x         ###   ########.fr       */
+/*   Updated: 2022/09/28 03:08:08 by sjhony-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(int argc, char **argv)
 {
 	t_node *current;
-	//t_node *current_2;
+	t_node *current_2;
 	t_data data;
 
 	if (argc < 2)
@@ -23,13 +23,12 @@ int main(int argc, char **argv)
 
 	init(argc, argv, &data);
 
-	/* push_b(&data);
+	push_b(&data);
 	push_b(&data);
 	push_b(&data);
 	push_b(&data);
 
-	rotate_all(data.stack_a, data.stack_b);
-	//rotate_b(data.stack_b);
+	reverse_rotate_all(data.stack_a, data.stack_b);
 	if (data.stack_b != NULL)
 	{
 		current_2 = data.stack_b->list;
@@ -39,10 +38,8 @@ int main(int argc, char **argv)
 			current_2 = current_2->next;
 		}
 		ft_printf("size stack_b=%d\n", ft_size(data.stack_b->list));
-	} */
+	}
 	
-	//rotate_a(data.stack_a);
-	reverse_rotate_a(data.stack_a);
 	current = data.stack_a->list;
 	while (current)
 	{
@@ -53,10 +50,10 @@ int main(int argc, char **argv)
 	ft_free_nodes(&data.stack_a->list);
 	free(data.stack_a);
 
-	/* if (data.stack_b != NULL)
+	if (data.stack_b != NULL)
 	{
 		ft_free_nodes(&data.stack_b->list);
 		free(data.stack_b);
-	} */
+	}
 	return (0);
 }
