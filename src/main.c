0b41553a8ebecef4469795eea84ada08cdf56249
sjhony-x <sjhony-x@student.42sp.org.br>
2022-10-20 23:08:00 +0200

    return 1 if argc == 1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sjhony-x <sjhony-x@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:28:34 by sjhony-x          #+#    #+#             */
-/*   Updated: 2022/10/20 22:15:16 by sjhony-x         ###   ########.fr       */
+/*   Updated: 2022/10/20 23:07:26 by sjhony-x         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ int	main(int argc, char **argv)
 	t_data data;
 	int size;
 
+	if (argc == 1)
+		return (1);
 	init(argc, argv, &data);
-	if (validate(&data))
+	if (validate(&data) && argc > 2)
 	{
 		size = ft_size(data.stack_a->list);
 		if (size == 2)
@@ -32,6 +34,7 @@ int	main(int argc, char **argv)
 	else
 	{
 		ft_putstr_fd("Error!\n", 2);
+		free_stacks(&data);
 		exit(1);
 	}
 	return (0);
